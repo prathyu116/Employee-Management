7010c72741ade998ceb5a26c9474bd658b9b64c6
prathyu116 <prathyuprasad116@gmail.com>
2024-05-15 11:14:00 +0530

    json-server

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001';
+const BASE_URL = 'http://localhost:3000';
 
 export const fetchEmployees = () => {
     return axios.get(`${BASE_URL}/employees`);
